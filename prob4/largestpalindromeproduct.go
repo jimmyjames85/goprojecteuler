b1533c95f46dd595acbb19492165224bea8ee60c
jimmyjames <james.tappe@sendgrid.com>
2017-03-25 11:01:09 -0700

    prob4 finshed

--- prob4/largestpalindromeproduct.go
+++ prob4/largestpalindromeproduct.go
@@ -12,19 +12,77 @@ import "fmt"
 //
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
+// product is only used to send results through channels. a*b SHOULD equal p but is not enforced
+type product struct {
+	a int
+	b int
+	p int
+}
+
 func main() {
-	ch := make(chan (int))
-	go generateProductsOf3DigitNumbers(ch)
+	findAll := false
+	digits := 3
 
-	for {
-		p := <-ch
-		fmt.Printf("received: %d\n", p)
+	ch := make(chan (product))
+	go generateProductsOfNDigitNumbers(ch, digits)
 
-		if isPalindrome(p) {
-			fmt.Printf("FOUND IT\n")
-			return
+	var p product
+	var ok bool
+	for p, ok = <-ch; ok && (findAll || !isPalindrome(p.p)); p, ok = <-ch {
+		if findAll && isPalindrome(p.p) {
+			fmt.Printf("%v\n", p)
 		}
 	}
+
+	if !findAll {
+		if ok && isPalindrome(p.p) {
+			fmt.Printf("The largest palindrome made from the product of two %d-digit numbers is: %d * %d = %d\n", digits, p.a, p.b, p.p)
+		} else {
+			fmt.Printf("Failed to find palindrome.\n")
+		}
+	}
+
+}
+
+// generateProductsOfNDigitNumbers generates 999*999, 999*998, 998*998, 998*997, ... , 100*100
+func generateProductsOfNDigitNumbers(ch chan<- product, digits int) {
+
+	if digits < 0 {
+		close(ch)
+		return
+	}
+
+	start := 0
+	for i := 0; i < digits; i++ {
+		start = start*10 + 9
+	}
+	end := 1 + (start-9)/10
+
+	generateDecliningThreeDigitNumbers := func(ch chan<- int) {
+		for n := start; n >= end; n-- {
+			ch <- n
+		}
+		close(ch)
+	}
+
+	chA := make(chan (int))
+	chB := make(chan (int))
+	go generateDecliningThreeDigitNumbers(chA)
+	go generateDecliningThreeDigitNumbers(chB)
+
+	readA := true
+	a, aOk := <-chA
+	b, bOk := <-chB
+	for aOk && bOk {
+		ch <- product{a: a, b: b, p: a * b}
+		if readA {
+			a, aOk = <-chA
+		} else {
+			b, bOk = <-chB
+		}
+		readA = !readA
+	}
+	close(ch)
 }
 
 func isPalindrome(n int) bool {
@@ -39,47 +97,6 @@ func isPalindrome(n int) bool {
 		}
 	}
 	return true
-}
-
-func generateProductsOf3DigitNumbers(ch chan<- int) {
-
-	chA := make(chan (int))
-	chB := make(chan (int))
-
-	go generateDecliningThreeDigitNumbers(chA)
-	go generateDecliningThreeDigitNumbers(chB)
-
-	a := <-chA
-	b := <-chB
-
-	readA := true
-
-	// todo this should not check against 101
-	// todo but rather check if the channel is closed ?... need to look up how chans work
-	for a >= 101 && b >= 101 {
-		fmt.Printf("%d * %d = %d\n", a, b, a*b)
-		ch <- a * b
-
-		if readA {
-			a = <-chA
-		} else {
-			b = <-chB
-		}
-
-		readA = !readA
-	}
-
-	for n1 := 999; n1 > 99; n1-- {
-		for n2 := 999; n2 > 99; n2-- {
-			ch <- 2
-		}
-	}
-}
-
-func generateDecliningThreeDigitNumbers(ch chan<- int) {
-	for n := 999; n > 99; n-- {
-		ch <- n
-	}
 }
 
 func abs(n int) int {
